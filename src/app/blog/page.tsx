662ec75e9a0502fd9a05405bdea38e691ce5ba19
13x54n <laxman.rai.07.26@gmail.com>
2026-02-27 13:34:49 -0500

    blog: feb 27 blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -33,12 +33,8 @@ export default function Blog() {
         {blog.title}
       </Heading>
       <Column fillWidth flex={1} gap="40">
-        <Posts range={[1, 3]} columns="2" thumbnail direction="column" />
+        <Posts columns="3" thumbnail direction="column" />
         {/* <Mailchimp marginBottom="l" /> */}
-        <Heading as="h2" variant="heading-strong-xl" marginLeft="l">
-          Earlier posts
-        </Heading>
-        <Posts range={[4]} columns="2" />
       </Column>
     </Column>
   );
